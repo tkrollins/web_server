a59dbb7d3a712a089648b51c4aa27d5de3891827
Chun Chen <joris@cs130.org>
2019-01-27 22:43:08 +0000

    Add a sample Google mock test to our tests base and update CMakeLists accordingly. Plan to refactor the code in the next commit and see if there are easier way to implement them since gmock is kind of tricky to use
    Change-Id: Iefa35ff9eccb5b21ae58479c99cff0c167a4bff5

--- include/session.h
+++ include/session.h
@@ -12,28 +12,44 @@
 #include <iostream>
 #include <boost/bind.hpp>
 #include <boost/asio.hpp>
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
 
 using boost::asio::ip::tcp;
+using ::testing::_;
+using ::testing::Invoke;
 
 class session
 {
-public:
-  session(boost::asio::io_service& io_service)
-    : socket_(io_service){}
+	public:
+		session(boost::asio::io_service& io_service) : socket_(io_service) {};
+		tcp::socket& socket();
 
-  tcp::socket& socket();
+		void start();
+		virtual std::string render_response(std::string inputStr);
+	private:
+		void handle_read(const boost::system::error_code& error,
+			size_t bytes_transferred);
 
-  void start();
-  std::string render_response(std::string inputStr);
-  
-private:
-  void handle_read(const boost::system::error_code& error,
-      size_t bytes_transferred);
+		void handle_write(const boost::system::error_code& error);
 
-  void handle_write(const boost::system::error_code& error);
-
-  tcp::socket socket_;
-  enum { max_length = 1024 };
-  char data_[max_length];
-  bool parse_http_request(char* inputStr, char* requestBody);
+		tcp::socket socket_;
+		enum { max_length = 1024 };
+		char data_[max_length];
+		bool parse_http_request(char* inputStr, char* requestBody);
 };
+
+class MockSession : public session
+{
+	public:
+		MockSession(boost::asio::io_service& io_service) : session(io_service), real_(trueIOS)
+		{
+			ON_CALL(*this, render_response(_))
+				.WillByDefault(Invoke(&real_, &session::render_response));
+		}
+		MOCK_METHOD1(render_response, std::string(std::string inputStr));
+	private:
+		boost::asio::io_service trueIOS;
+		session real_;
+};
+
