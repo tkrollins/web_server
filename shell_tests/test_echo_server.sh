f613f64541d3b13f82cf51b40f58548fd7aa20c9
Chun Chen <joris@cs130.org>
2019-01-28 23:20:45 +0000

    modify test shell script
    Change-Id: Ifdbbfdf5dd84dabcb6e46ed54527abf30fd8828d

--- shell_tests/test_echo_server.sh
+++ shell_tests/test_echo_server.sh
@@ -13,7 +13,7 @@ function test_echo
     fi
 }
 
-../server ../test_config > /dev/null &
+../build/bin/server ./test_config > /dev/null &
 test_echo $SERVER_PID
 echo "Response body is NOT correct"
 kill $SERVER_PID
