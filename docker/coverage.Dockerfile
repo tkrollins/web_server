f1726d92cb0c55053d0d553bd6a7ba0aaf958792
Trevor Rollins <trevor.rollins@cs130.org>
2019-01-28 23:45:20 +0000

    Trying to move whole dir.
    Change-Id: Ib8ba0eb972f71d2c04b0cac929cc940747d24150

--- docker/coverage.Dockerfile
+++ docker/coverage.Dockerfile
@@ -4,9 +4,11 @@ FROM bigbear:base as builder
 
 # Share work directory
 COPY . /usr/src/project
-COPY --from=builder /usr/src/project/build/bin/server .
-COPY --from=builder /usr/src/project/shell_tests/test_config .
 WORKDIR /usr/src/project/build_coverage
+
+COPY --from=builder /usr/src/project/build/bin/server .
+COPY --from=builder /usr/src/project/shell_tests .
+
 
 # Build and test
 RUN cmake -DCMAKE_BUILD_TYPE=Coverage ..
--- shell_tests/test_echo_server.sh
+++ shell_tests/test_echo_server.sh
@@ -2,8 +2,8 @@
 
 function test_echo
 {
-    response="actual_response"
-    expected_response="expected_response"
+    response="./shell_tests/actual_response"
+    expected_response="./shell_tests/expected_response"
     curl -s localhost:5566 > $response
     if cmp -s $response $expected_response; then
         echo "Response body is correct"
@@ -13,7 +13,7 @@ function test_echo
     fi
 }
 
-../build/bin/server ./test_config > /dev/null &
+./server ./shell_tests/test_config > /dev/null &
 test_echo $SERVER_PID
 echo "Response body is NOT correct"
 kill $SERVER_PID
