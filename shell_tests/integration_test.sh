a33b68825dc756d612de3d0d831ef92cf3891299
Trevor Rollins <trevor.rollins@cs130.org>
2019-02-04 04:19:07 +0000

    Added on to the integration test for new test cases.
    Change-Id: Ib1a842ab9e28b1f1f9f7926f29868c1aa390fcae

---
+++ shell_tests/integration_test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function test_response
+{
+    response=$(curl -s localhost:5566$2)
+    if [ "$response" != "$3" ]; then
+        echo "$2 Response is NOT correct"
+        echo -n "$response" > response.txt
+        echo -n "$expected_response" > expected.txt
+        kill $1
+        exit 1
+    fi
+}
+
+../build/bin/server ./test_config > /dev/null &
+SERVER_PID=$!
+sleep .5
+
+# test echo response
+URI="/echo"
+expected_response=$'GET /echo HTTP/1.1\r\nHost: localhost:5566\r\nUser-Agent: curl/7.58.0\r\nAccept: */*\r\n\r'
+test_response $SERVER_PID $URI "$expected_response"
+echo "Echo response is correct"
+
+# test bad-request response
+URI="/bad"
+expected_response=$'400 Error: Bad request'
+test_response $SERVER_PID $URI "$expected_response"
+echo "Bad-request response is correct"
+
+# test static .txt file response
+URI="/static/bearlist.txt"
+expected_response=$'Here is a list of bears:\n\nGrizzly Bear\nPanda Bear\nGummy Bear\nKoala Bear\nYogi Bear\nSmokey The Bear\nPolar Bear'
+test_response $SERVER_PID $URI "$expected_response"
+echo "Static .txt response is correct"
+
+kill $SERVER_PID
+exit 0
