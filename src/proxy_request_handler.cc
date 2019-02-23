f3075eb7d4bd74b2b945c7bc656b491aa5e81f33
yanzhao <zephyrwyz@cs130.org>
2019-02-23 22:33:39 +0000

    send a request to a dest server and receive good response
    Change-Id: I2362cb2c68ce437c1aea55765f979b83f061fe33

--- src/proxy_request_handler.cc
+++ src/proxy_request_handler.cc
@@ -4,14 +4,13 @@
 #include "proxy_request_handler.h"
 
 ProxyRequestHandler::ProxyRequestHandler(
-    const std::unordered_map<std::string, std::string> staticPathMap, std::string destPath):
+    const std::unordered_map<std::string, std::string> proxyPathMap, std::string destPath):
     RequestHandler()
 {
-    validURIMap = staticPathMap;
+    validURIMap = proxyPathMap;
     dest = validURIMap["dest"];
     std::cout << "destinated proxy address: " +  dest << std::endl;
 }
-
 
 std::unique_ptr<RequestHandler> ProxyRequestHandler::create(const NginxConfig& config, const std::string& destPath){
     ProxyRequestHandler proxyHandler(config.getFlatParameters(), destPath);
@@ -20,6 +19,77 @@ std::unique_ptr<RequestHandler> ProxyRequestHandler::create(const NginxConfig& c
 
 std::unique_ptr<HttpResponse> ProxyRequestHandler::HandleRequest(const HttpRequest &request)
 {
+    //test for sending request to dest server
+    boost::asio::io_service io_service;
+
+    // Get a list of endpoints corresponding to the server name.
+    tcp::resolver resolver(io_service);
+    tcp::resolver::query query("www.ucla.edu", "http");
+    tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+
+    // Try each endpoint until we successfully establish a connection.
+    tcp::socket socket(io_service);
+    boost::asio::connect(socket, endpoint_iterator);
+
+    // Form the request. We specify the "Connection: close" header so that the
+    // server will close the socket after transmitting the response. This will
+    // allow us to treat all data up until the EOF as the content.
+    boost::asio::streambuf req;
+    std::ostream request_stream(&req);
+    request_stream << "GET " << "/" << " HTTP/1.0\r\n";
+    request_stream << "Host: " << "www.ucla.edu" << "\r\n";
+    request_stream << "Accept: */*\r\n";
+    request_stream << "Connection: close\r\n\r\n";
+
+    // Send the request.
+    boost::asio::write(socket, req);
+
+    // Read the response status line. The response streambuf will automatically
+    // grow to accommodate the entire line. The growth may be limited by passing
+    // a maximum size to the streambuf constructor.
+    boost::asio::streambuf response;
+    boost::asio::read_until(socket, response, "\r\n");
+
+    // Check that response is OK.
+    std::istream response_stream(&response);
+    std::string http_version;
+    response_stream >> http_version;
+    unsigned int status_code;
+    response_stream >> status_code;
+    std::string status_message;
+    std::getline(response_stream, status_message);
+    if (!response_stream || http_version.substr(0, 5) != "HTTP/")
+    {
+      std::cout << "Invalid response\n";
+    }
+    if (status_code != 200)
+    {
+      std::cout << "Response returned with status code " << status_code << "\n";
+    }
+
+    // Read the response headers, which are terminated by a blank line.
+    boost::asio::read_until(socket, response, "\r\n\r\n");
+
+    // Process the response headers.
+    std::string header;
+    while (std::getline(response_stream, header) && header != "\r")
+        std::cout << header << "\n";
+    std::cout << "\n";
+
+    // Write whatever content we already have to output.
+    if (response.size() > 0)
+        std::cout << &response;
+
+    // Read until EOF, writing data to output as we go.
+    boost::system::error_code error;
+    while (boost::asio::read(socket, response,
+           boost::asio::transfer_at_least(1), error))
+       std::cout << &response;
+    if (error != boost::asio::error::eof)
+       throw boost::system::system_error(error);
+
+
+    //currently return 200, do nothing
     status = 200;
     std::string body = "Proxy Test\r\n";
     std::string contentLengthStr = std::to_string(body.length());
