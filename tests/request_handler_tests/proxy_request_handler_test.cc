2aadfa01062e0579f3ef87ffacca92ea8632384a
yanzhao <zephyrwyz@cs130.org>
2019-02-25 19:23:55 +0000

    modify the test to fit the newest proxy handler
    Change-Id: Icd95e7b0caaac06c424651784a843f86c6ed60a2

--- tests/request_handler_tests/proxy_request_handler_test.cc
+++ tests/request_handler_tests/proxy_request_handler_test.cc
@@ -35,15 +35,15 @@ TEST_F(ProxyRequestHandlerTest, ProxyRedirect) {
     // The test will only pass after the server has been redeployed to incorporate redirect handler.
     // TODO: Change "redirectMap" to the fisrt definition when server deployment is successful
 
-    // unordered_map<string, string> redirectMap { {"dest", "static.bigbear.cs130.org/redirect"} };
-    unordered_map<string, string> redirectMap { {"dest", "live.com"} };
-    
+    unordered_map<string, string> redirectMap { {"dest", "static.bigbear.cs130.org/redirect"} };
+    //unordered_map<string, string> redirectMap { {"dest", "live.com"} };
+
     ProxyRequestHandler proxyHandler(redirectMap, "../..");
     req.requestURI = "/moved_echo";
     std::unique_ptr<HttpResponse> res = proxyHandler.HandleRequest(req);
     cout << "Status Code" <<res -> errorCode << endl;
-    
-    EXPECT_TRUE(res->errorCode == 301);
-    // TODO: Change errorCode assertion to 200 when server deployment is successful 
+
+    EXPECT_TRUE(res->errorCode == 200);
+    // TODO: Change errorCode assertion to 200 when server deployment is successful
     // EXPECT_TRUE(res->errorCode == 200);
 }
