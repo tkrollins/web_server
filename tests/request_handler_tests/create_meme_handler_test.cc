837ebd5dd15baddecb9059f75e34d43732e23284
Trevor Rollins <trevor.rollins@cs130.org>
2019-03-05 03:25:50 +0000

    Added unit test that should test for thread safety of createMemeHandler.
    Change-Id: I0444e9679ab6ba639b224154a05c406342960455

--- tests/request_handler_tests/create_meme_handler_test.cc
+++ tests/request_handler_tests/create_meme_handler_test.cc
@@ -3,6 +3,78 @@
 #include "http_request.h"
 #include "meme_db.h"
 #include "handler_manager.h"
+#include <set>
+#include "base64.h"
+#include <thread>
+
+void createMemes(std::set<std::string>* IDs, int i)
+{
+    NginxConfig config;
+    HandlerManager manager;
+    std::unique_ptr<RequestHandler> createMemeHandler = manager.createByName("createMeme", config, "");
+
+    int stop = i + 100;
+
+    // Add 100 memes to the database
+    for(; i<stop; i++)
+    {
+        char buff[50];
+        sprintf(buff, "image=%d&top=%d&bottom=%d", i, i+1, i+2);
+
+        HttpRequest request;
+        request.requestMethod = httpRequestEnum::POST;
+        request.requestBody = buff;
+
+        std::unique_ptr<HttpResponse> response = createMemeHandler -> HandleRequest(request);
+
+        char buff2[50];
+        sprintf(buff2, "%dtop:%dbottom:%d", i, i+1, i+2);
+        std::string paramsString = buff2;
+        std::string id = base64_encode(reinterpret_cast<const unsigned char*>(paramsString.c_str()), paramsString.length());
+
+        // Keep track of meme ids added to database
+        IDs->insert(id);
+    }
+}
+
+bool compareIDs(const std::set<std::string>& subsetIDs, std::set<std::string>& IDs)
+{
+    for(auto& id : subsetIDs)
+    {
+        if(IDs.count(id) == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+TEST(CreateMemeHandlerTest, TestThreadSafety)
+{
+    MemeDB database("/usr/src/projects/bigbear/build/tmp_db");
+
+    std::set<std::string> IDs_1;
+    std::set<std::string> IDs_2;
+    std::set<std::string> IDs_all;
+
+    // Each thread will add 100 memes to database
+    // Idea is they should be storing memes simultaneously
+    std::thread thread1(createMemes, &IDs_1, 1);
+    std::thread thread2(createMemes, &IDs_2, 200);
+
+    thread1.join();
+    thread2.join();
+
+    // All meme IDs stored in database
+    IDs_all = database.getIDs();
+
+    // No IDs should have gotten lost or overwritten
+    EXPECT_TRUE(compareIDs(IDs_1, IDs_all));
+    EXPECT_TRUE(compareIDs(IDs_2, IDs_all));
+
+    database.clear();
+}
+
 
 TEST(CreateMemeHandlerTest, CreateWithPostRequest) {
     NginxConfig config;
